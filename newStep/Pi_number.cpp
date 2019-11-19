f9e8b5d60ee28334dfb08ed82ef48388fc4c30c2
Alek <al.semakin@gmail.com>
2019-11-19 22:10:25 +0300

    Added time.h

--- newStep/Pi_number.cpp
+++ newStep/Pi_number.cpp
@@ -13,27 +13,32 @@ PI = 1/(2^6) summ(from n = 0 to n = inf) ( (-1)^n )/( 2^10n ) *
 #include <iostream>
 #include <cmath>
 
+using namespace std;
 
 void Bellard(int N)
 {
-	long double PI = 0;
+	long double PI = 0, sum = 0;
 	clock_t t = clock();
 
 	for(int n = 0; n < N; n++)
 	{
-		PI = 0;
+		
 	}
+	PI = 1/pow(2,6)*sum;
+	t = (clock() - t)/CLK_TCK;
 
-
+	cout << "\n\n\t" << "Using equation of Bellard Pi-number equal is " 
+	 <<PI  << endl;
+	cout << "\tSpended time is " << t << "\n\n" << endl;
 }
 
-using namespace std;
+
+
 
 int main()
 {
 	long double pi = 0;
-
-
+	Bellard(1);
 
 
 	return 0;
