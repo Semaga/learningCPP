837dc5b458207b674219960d703f26e7f7d54b64
Alek <al.semakin@gmail.com>
2019-11-19 21:49:31 +0300

    Try to released calculation PI-number using equation of Bellard

--- newStep/Pi_number.cpp
+++ newStep/Pi_number.cpp
@@ -1,1 +1,26 @@
 // This programme for calculation of pi-number
+#include <time.h>
+#include <iostream>
+/*
+Реализация формулы Белларда:
+
+PI = 1/(2^6) summ(from n = 0 to n = inf) ( (-1)^n )/( 2^10n ) *
+		( (-2)^5/(4n+1) -1/(4n+3) + 2^8/(10n+1) -2^6/(10n+3) - 2^2/ (10n+5) - 2^2/(10n+7) + 1/(10n+9) )
+
+*/
+
+void Bellard(int N){
+	clock_t t = clock();
+}
+
+using namespace std;
+
+int main()
+{
+	long double pi = 0;
+
+
+
+
+	return 0;
+}
