216c054a518b1a73c78fb2b32d57653441a40878
Alek <al.semakin@gmail.com>
2019-12-08 11:33:02 +0300

    punpun

---
+++ PC/integral.c
@@ -0,0 +1,19 @@
+# include "integral.h"
+
+double f(double x)
+{
+	return x;
+}
+
+double integrate(double a, double b, int n)
+{
+	double res;
+	double h;
+	int i;
+
+	h = (b-a)/n;
+	res = 0.5 * ( f(a) + f(b) )*h;
+	for( i = 1; i < n; i++)
+		res += f(a+i*h)*h;
+	return res;
+}
