770e4db36d5cfc8f15a05887418a651f64c66e21
Alek <al.semakin@gmail.com>
2019-12-08 17:24:08 +0300

    Write using funcion in file

--- PC/MultiMode/UsingProcess/process.c
+++ PC/MultiMode/UsingProcess/process.c
@@ -3,3 +3,12 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include "integral.h"
+
+static double a = 0;
+static double b = 1;
+static int n = 100000000;
+
+static int from_root[2];
+
+static int to_root[2];
+
