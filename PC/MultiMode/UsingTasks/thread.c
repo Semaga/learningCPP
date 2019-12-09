a8d757b1bd4293dbdc2d04032b117070b137e9fd
Alek <al.semakin@gmail.com>
2019-12-09 20:46:40 +0300

    Have some problems "2 warnings generated."

--- PC/MultiMode/UsingTasks/thread.c
+++ PC/MultiMode/UsingTasks/thread.c
@@ -29,3 +29,42 @@ void * process_function (void *pa)
 		pthread_mutex_unlock (&total_mutex);
 		return 0;
 	}
+
+int int main(int argc, char  *argv[])
+	{
+		pthread_t * threads;
+		int i;
+
+		if (argc != 2)
+			{
+				printf("Usage: %s <instances>\n", argv[0]);
+				return 1;
+			}
+
+		p = (int) strtol (argv[1], 0, 10);
+
+		if (!(threads = (pthread_t*) malloc (p * sizeof (pthread_t))))
+			{
+				fprintf(stderr, "Not enough memory!\n");
+				return 1;
+			}
+		for (i = 0; i < p; i++)
+			{
+				if (pthread_create (threads+i, 0, process_function, (void*)i))
+					{
+						fprintf(stderr, "Cannot create thread #%d!\n", i);
+						return 2;
+					}
+			}
+		for ( i = 0; i < p; i++)
+			{
+				if(pthread_join (threads[i],0))
+					fprintf(stderr, "Cannot wait thread #%d!\n", i);
+			}	
+
+		free(threads);
+
+		printf("integral from %lf to %lf = %.18lf\n",a, b, total);
+
+		return 0;
+	}
