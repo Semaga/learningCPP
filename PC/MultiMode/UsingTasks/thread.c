3b91b42ad5d65775db190afd03dc64664540fe90
Alek <al.semakin@gmail.com>
2019-12-09 20:24:06 +0300

    Write process_funcktion

--- PC/MultiMode/UsingTasks/thread.c
+++ PC/MultiMode/UsingTasks/thread.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include "integral.h"
+
+static double a = 0;
+static double b = 1;
+static int n = 100000000;
+
+static double total = 0;
+
+static pthread_mutex_t total_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static int p;
