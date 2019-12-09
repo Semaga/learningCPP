0ba1cac2b023f84a99541da95c4f8d398bd49021
Alek <al.semakin@gmail.com>
2019-12-09 20:34:06 +0300

    Write main fucntion without comments, ups

--- PC/MultiMode/UsingTasks/thread.c
+++ PC/MultiMode/UsingTasks/thread.c
@@ -12,3 +12,20 @@ static double total = 0;
 static pthread_mutex_t total_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static int p;
+
+void * process_function (void *pa)
+	{
+		int my_rank = (int)pa;
+		double len = (b-a)/p;
+		int local_n = n/p;
+		double local_a = a + my_rank* len;
+		double local_b = local_a + len;
+		double integral;
+
+		integral = integate (local_a, local_b, local_n);
+
+		pthread_mutex_lock (&total_mutex);
+		total+= integral;
+		pthread_mutex_unlock (&total_mutex);
+		return 0;
+	}
