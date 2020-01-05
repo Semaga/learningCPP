a4c64171d57f6a263ae87cd646407390ed6d6533
Alek <al.semakin@gmail.com>
2020-01-05 12:39:11 +0300

    Add split-function

--- Sort/mkFile.cpp
+++ Sort/mkFile.cpp
@@ -6,30 +6,52 @@
 
 using namespace std;
 
-void MakeRandomNumber(const string &fileName);
-
-
+vector <string> split (string &s, char delimiter = ' ');
+void WriteRandomNumber(const string &fileName);
+void DoSortFromFile(const string &fileName, vector &v);
 
 int main(int argc, char const *argv[])
 {
 	string fileName = "dRawFile.txt";
-	MakeRandomNumber(fileName);
-
+	WriteRandomNumber(fileName);
+	vector<int> v;
 	return 0;
 }
 
-void MakeRandomNumber(const string &fileName)
+vector <string> split (string &s, char delimiter = ' ')
 {
-	int x;
-	ofstream input(fileName); //ofstream using for write to file
-	if(input)
+	stringstream ss(s);
+	string item;
+	vector <string> tokens;
+	while (getline(ss,item, delimiter))
+		tokens.push_back(item);
+	return tokens;
+}
+
+
+void WriteRandomNumber(const string &fileName)
+{
+	ofstream fout(fileName); //ofstream using for write to file
+	if(fout)
 	{
 		cout << "\tFile " << fileName << " is open ..." << endl;
 		cout << "\t\tWrite to " << fileName << endl;
 		for(int i = 0; i < 10000; i++)
-  		input << rand() << " ";
-		input.close();
+  		fout << rand() << " ";
+		fout.close();
 		cout << "\tFile " << fileName << " was closed ..." << endl;
+	}else
+		cout << "\tError" << "\n\tfile " << fileName << " not found" << endl;
+}
+
+void DoSortFromFile(const string &fileName, vector &v)
+{
+	ifstream fin(fileName); //
+	if(fin)
+	{
+		string line;
+		getline(fin, line);
+
 	}else
 		cout << "\tError" << "\n\tfile " << fileName << " not found" << endl;
 }
