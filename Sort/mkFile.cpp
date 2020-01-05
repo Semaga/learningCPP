6665671a2c3e1e574092df97df8e27020a67d105
Alek <al.semakin@gmail.com>
2020-01-05 12:03:19 +0300

    Add MakeRandomNumber function. This function generate random number and write to file

--- Sort/mkFile.cpp
+++ Sort/mkFile.cpp
@@ -2,13 +2,27 @@
 
 #include <fstream>
 #include <string>
+#include <iostream>
 
 using namespace std;
+
+void MakeRandomNumber(ofstream &fout)
+{
+	int x;
+	cout << "RAND_MAX = " << RAND_MAX << endl; // константа, хранящая максимальный предел из интервала случайных чисел
+  for(int i = 0; i< 10000; i++)
+  {	
+  	x = rand();
+  	cout << "random number = " << x << endl;
+  	fout << x << endl;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
 	ofstream fout ("dFile.txt");
 	fout << "Work with files" << endl;
+	MakeRandomNumber(fout);
 	fout.close();
 
 	return 0;
