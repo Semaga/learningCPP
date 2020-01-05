cb779f2de5d8653a9aead90eb6738f2237b8fcf7
Alek <al.semakin@gmail.com>
2020-01-05 11:47:10 +0300

    First try to write something to file

--- Sort/mkFile.cpp
+++ Sort/mkFile.cpp
@@ -1,4 +1,15 @@
 //This file make files with random quantity numbers
 
-#include <sstream>
+#include <fstream>
 #include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ofstream fout ("dFile.txt");
+	fout << "Work with files" << endl;
+	fout.close();
+
+	return 0;
+}
