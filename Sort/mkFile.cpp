aecfe31320897cabe5603b29ff748a43ce09a376
Alek <al.semakin@gmail.com>
2020-01-05 12:24:35 +0300

    The code has been rewritten. Function was shift to end file

--- Sort/mkFile.cpp
+++ Sort/mkFile.cpp
@@ -6,22 +6,9 @@
 
 using namespace std;
 
-void MakeRandomNumber(const string &fileName)
-{
-	int x;
-	ofstream input(fileName); //ofstream using for write to file
-	if(input)
-	{
-		for(int i = 0; i < 10000; i++)
-  	{		
-  		x = rand();
-  		cout << "random number = " << x << endl;
-  		input << x << endl;
-		}	
-		input.close();
-	}else
-		cout << "\tError" << "\n\tfile " << fileName << " not found" << endl;
-}
+void MakeRandomNumber(const string &fileName);
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -29,4 +16,20 @@ int main(int argc, char const *argv[])
 	MakeRandomNumber(fileName);
 
 	return 0;
+}
+
+void MakeRandomNumber(const string &fileName)
+{
+	int x;
+	ofstream input(fileName); //ofstream using for write to file
+	if(input)
+	{
+		cout << "\tFile " << fileName << " is open ..." << endl;
+		cout << "\t\tWrite to " << fileName << endl;
+		for(int i = 0; i < 10000; i++)
+  		input << rand() << " ";
+		input.close();
+		cout << "\tFile " << fileName << " was closed ..." << endl;
+	}else
+		cout << "\tError" << "\n\tfile " << fileName << " not found" << endl;
 }
